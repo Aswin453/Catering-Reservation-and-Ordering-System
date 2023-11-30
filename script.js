637b3e092eedfc2a3c25aad8a7292b9996d71d94
Aswin <140883564+Aswin453@users.noreply.github.com>
2023-11-30 07:44:38 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+document.getElementById('reservationForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+    // Handle reservation form submission logic
+    alert('Reservation submitted!');
+  });
+  
+  document.getElementById('orderForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+    // Handle order form submission logic
+    alert('Order placed!');
+  });
+s  
